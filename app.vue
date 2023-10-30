004b3bc4ebd68ad2c98d4d3ae141c42c46eca890
Luke <ldary@redhat.com>
2023-10-30 10:26:38 -0400

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,6 +2,6 @@
   <div>
     <h1>Nuxt.js proof-of-concept</h1>
     <h2>Delivered by OpenShift</h2>
-    <p>Running on the Developer Sandbox</p>
+    <p>Running on the <a href="https://developers.redhat.com/developer-sandbox">Developer Sandbox</a></p>
   </div>
 </template>
